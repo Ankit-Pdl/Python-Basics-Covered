c305f56973c05007f6c75efb8a27fc803a8dd10d
Ankit Poudel <pdlankit2000@gmail.com>
2026-05-02 17:11:10 +0545

    Touched some basics of Lists

---
+++ Lists.py
@@ -0,0 +1,11 @@
+# program to illustrate students in a class: 
+
+st1 = "Amar"
+st2 = "Sweety"
+st3 = "Ria"
+st4 = "Unsa"
+
+
+# better way:
+students = ["Amar", "Sweety","Ria","Unsa"]
+print(students[0])
