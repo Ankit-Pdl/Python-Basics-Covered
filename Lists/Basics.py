9686df4cab2710b95d26d62a962136a6e4d6133e
Ankit Poudel <pdlankit2000@gmail.com>
2026-05-03 14:17:54 +0545

    List teaching completed!

---
+++ Lists/Basics.py
@@ -0,0 +1,22 @@
+name1 = "anamika"
+nam2 = "Rahul"
+
+
+
+# name20443 = "Place"
+
+# students = ["anamika", "Rahul","Anmol"]
+# print(students[0])
+
+
+# students.append("Ankit")
+# print(students)
+
+# students.insert(1,"Samriddhi")
+
+# print(students)
+
+list1 = [1,2,3]
+
+list2 = list1[:]
+print(list2)
