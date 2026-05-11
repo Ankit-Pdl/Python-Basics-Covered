7d4f1b838f32eee92cf54115108070292af74352
Ankit Poudel <pdlankit2000@gmail.com>
2026-05-11 22:00:51 +0545

    Taught conditionals

---
+++ ifElseConditions.py/basics.py
@@ -0,0 +1,35 @@
+students = [1,2,3,4,5]
+
+# for x in students:
+#     if x %2 ==0:
+#         print(x)
+
+
+# age = 21
+# if age>=21:
+#    print("Eligible to buy beer!")
+
+
+#! if statement works only if the condition is true!
+
+# if True:
+#     print("Hello world")
+#     print("Anamika")
+# age = 56
+# if age>18:
+#    if age>=55:
+#       print(f"{age} is high!")
+
+# marks = 55
+
+# if marks>90:
+#     print("Genius")
+# elif marks>=70 and marks<=89:
+#     print("Average student")
+# else:
+#     print("You need to work hard")    
+
+a= 5
+b= 454
+
+if a<b : print("a is less")
